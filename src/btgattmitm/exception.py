ea8856b762cb82653b8b72b548473987c903b139
anetczuk <anetczuk@o2.pl>
2018-09-24 01:47:39 +0200

    Add exception

--- src/btgattmitm/exception.py
+++ src/btgattmitm/exception.py
@@ -23,3 +23,14 @@ class InvalidValueLengthException(dbus.exceptions.DBusException):
 class FailedException(dbus.exceptions.DBusException):
     _dbus_error_name = 'org.bluez.Error.Failed'
     
+
+class InvalidStateError(Exception): 
+  
+    # Constructor or Initializer 
+    def __init__(self, value): 
+        self.value = value 
+  
+    # __str__ is to print() the value 
+    def __str__(self): 
+        return(repr(self.value)) 
+
